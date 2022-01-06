0a11ad7d9f1336829cd45f1462d3a209de5a06c1
RedouaneAmour <amour@etud.insa-toulouse.fr>
2022-01-06 16:57:58 +0100

    central brain project

---
+++ CentralBrain/CentralBrainMS/src/main/java/fr/insa/mas/CentralBrainMS/CentralBrainMsApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.mas.CentralBrainMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralBrainMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralBrainMsApplication.class, args);
+	}
+
+}
---
+++ CentralBrain/CentralBrainMS/src/test/java/fr/insa/mas/CentralBrainMS/CentralBrainMsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.mas.CentralBrainMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralBrainMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
